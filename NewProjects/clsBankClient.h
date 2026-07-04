82455f6aaadaf10b4f9ea70cef8b3a6a2beb89ca
Burhan Haddad <burhanhaddad5@gmail.com>
2026-07-04 15:58:36 +0300

    Added some properties and functions in clsBankClient

--- NewProjects/clsBankClient.h
+++ NewProjects/clsBankClient.h
@@ -12,10 +12,31 @@ class clsBankClient : public clsPerson
 {
 private:
 	enum enMode { EmptyMode = 0, UpdateMode = 1 };
-
+	enMode _Mode;
 	string _AccountNumber;
 	string _PinCode;
 	float _AccountBalance;
+
+	static clsBankClient _ConvertLineToClientObject(string Line, string Separator) {
+
+		vector<string>vClientData;
+		vClientData = clsString::Split(Line, Separator);
+
+		return clsBankClient(enMode::UpdateMode, vClientData[0], vClientData[1], vClientData[2], vClientData[3], vClientData[4], vClientData[5], stod(vClientData[6]));
+	}
+
+
+
+
+
+
+	clsBankClient(enMode Mode, string FirstName, string LastName, string Email, string Phone, string AccountNumber, string PinCode, float AccountBalance)
+		:clsPerson(FirstName, LastName, Email, Phone) 
+	{
+		_Mode = Mode;
+		_AccountNumber = AccountNumber;
+		_AccountBalance = AccountBalance;
+	}
 
 
 };
