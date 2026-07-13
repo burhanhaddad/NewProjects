5a883e696deeee21a6da058b2a30ea089f32d9f6
Burhan Haddad <you@example.com>
2026-07-13 10:55:36 -0900

    added clsClientListScreen class

--- NewProjects/NewProjects.cpp
+++ NewProjects/NewProjects.cpp
@@ -6,7 +6,11 @@
 #include"clsInputValidate.h"
 #include"clsDate.h"
 #include"clsBankClient.h"
+#include"clsMainScreen.h"
+
 using namespace std;
+
+
 void ReadClientInfo(clsBankClient& Client) {
 
     cout << "\nEnter FirstName: ";
@@ -137,51 +141,8 @@ void DeleteClient() {
 
 }
 
-void PrintClientRecordLine(clsBankClient Client)
-{
 
-    cout << "| " << setw(15) << left << Client.AccountNumber();
-    cout << "| " << setw(20) << left << Client.FullName();
-    cout << "| " << setw(12) << left << Client.Phone;
-    cout << "| " << setw(25) << left << Client.Email;
-    cout << "| " << setw(10) << left << Client.PinCode;
-    cout << "| " << setw(12) << left << Client.AccountBalance;
 
-}
-
-void ShowClientsList()
-{
-
-    vector <clsBankClient> vClients = clsBankClient::GetClientsList();
-
-    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
-    cout << "\n_____________________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    cout << "| " << left << setw(15) << "Accout Number";
-    cout << "| " << left << setw(20) << "Client Name";
-    cout << "| " << left << setw(12) << "Phone";
-    cout << "| " << left << setw(25) << "Email";
-    cout << "| " << left << setw(10) << "Pin Code";
-    cout << "| " << left << setw(12) << "Balance";
-    cout << "\n_____________________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    if (vClients.size() == 0)
-        cout << "\t\t\t\tNo Clients Available In the System!";
-    else
-
-        for (clsBankClient Client : vClients)
-        {
-
-            PrintClientRecordLine(Client);
-            cout << endl;
-        }
-
-    cout << "\n_____________________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-}
 
 
 void PrintClientRecordBalanceLine(clsBankClient Client)
@@ -229,6 +190,5 @@ void ShowTotalBalances()
 
 int main()
 {
-    ShowClientsList();
-    ShowTotalBalances();
+    clsMainScreen::ShowMainMenue();
 }
