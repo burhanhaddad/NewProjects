cb59681fdb67fe9f1a23da91697de559e20ac93d
Burhan Haddad <you@example.com>
2026-07-13 02:14:43 -0900

    Added two clsMainScreen class and some funtions

---
+++ NewProjects/clsMainScreen.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsInputValidate.h"
+#include <iomanip>
+
+using namespace std;
+
+class clsMainScreen :protected clsScreen
+{
+private:
+
+};
+
